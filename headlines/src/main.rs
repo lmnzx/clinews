2861ab45bf0b8d866e46588e1fc3467f25370402
Sayan Mallick <snyxmk@gmail.com>
2022-02-10 20:13:46 +0530

    basic gui setup

--- headlines/src/main.rs
+++ headlines/src/main.rs
@@ -1,11 +1,54 @@
-use eframe::{egui::CentralPanel, epi::App, epi::NativeOptions, run_native};
+use eframe::{
+    egui::{CentralPanel, ScrollArea, Vec2},
+    epi::App,
+    epi::NativeOptions,
+    run_native,
+};
 
-struct Headlines;
+struct Headlines {
+    articles: Vec<NewsCardData>,
+}
+
+impl Headlines {
+    fn new() -> Headlines {
+        let iter = (0..20).map(|i| NewsCardData {
+            title: format!("Headline {}", i),
+            description: format!("This is the description of headline {}", i),
+            url: format!("https://www.rust-lang.org/"),
+        });
+        Headlines {
+            articles: Vec::from_iter(iter),
+        }
+    }
+}
+
+struct NewsCardData {
+    title: String,
+    description: String,
+    url: String,
+}
 
 impl App for Headlines {
+    fn setup(
+        &mut self,
+        ctx: &eframe::egui::CtxRef,
+        _frame: &eframe::epi::Frame,
+        _storage: Option<&dyn eframe::epi::Storage>,
+    ) {
+        self.configure_fonts(ctx);
+    }
+
+    fn configure_fonts(&self, ctx: &eframe::egui::CtxRef) {}
+
     fn update(&mut self, ctx: &eframe::egui::CtxRef, _frame: &eframe::epi::Frame) {
         CentralPanel::default().show(ctx, |ui| {
-            ui.label("Hello, world!");
+            ScrollArea::vertical().show(ui, |ui| {
+                for article in &self.articles {
+                    ui.label(article.title.clone());
+                    ui.label(article.description.clone());
+                    ui.label(article.url.clone());
+                }
+            })
         });
     }
 
@@ -15,7 +58,8 @@ impl App for Headlines {
 }
 
 fn main() {
-    let app = Headlines;
-    let win_options = NativeOptions::default();
+    let app = Headlines::new();
+    let mut win_options = NativeOptions::default();
+    win_options.initial_window_size = Some(Vec2::new(540., 960.));
     run_native(Box::new(app), win_options)
 }
